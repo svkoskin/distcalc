60419beae31276ade4861cfe3873858fac3b8700
Sami Koskinen <sami.koskinen@helsinki.fi>
2016-02-16 22:14:23 +0200

    Working server-side solution for single calculation at a time

---
+++ calculator.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2.7
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.debug = True
+
+def is_a_number(arg):
+    try:
+        float(arg)
+    except ValueError:
+        return False
+
+    return True
+
+def calculate(arg1_str, arg2_str, op):
+    # Caller will catch ValueErrors raised by this
+    arg1 = float(arg1_str)
+    arg2 = float(arg2_str)
+
+    if op == "+":
+        return arg1 + arg2
+    elif op == "-":
+        return arg1 - arg2
+    elif op == "*":
+        return arg1 * arg2
+    elif op == "/":
+        return arg1 / arg2
+    else:
+        raise ValueError("Allowed operators are +, -, * and /")
+
+@app.route("/")
+def calculator_page():
+    errors = []
+    results = []
+
+    try: 
+        arg1 = request.args["arg1"]
+        arg2 = request.args["arg2"]
+        op = request.args["op"]
+        result = "{} {} {} = {}".format(arg1, op, arg2, calculate(arg1, arg2, op))
+        results.append(result)
+
+    except KeyError:
+        # We didn't get arguments and that's fine. (for instance, on the first
+        # page load this will happen)
+        pass
+
+    except ValueError as e:
+        # We got some invalid values from the user who should be informed in 
+        # returned page.
+        errors.append(str(e))
+
+    return render_template("calculator.html", results=results, errors=errors)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
