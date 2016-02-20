bdfcdc18d4ef9ca162e9234999430b07cbce1954
Sami Koskinen <sami.koskinen@helsinki.fi>
2016-02-20 14:51:09 +0200

    Separate calculations from Web stuff

--- calculator.py
+++ calculator.py
@@ -8,59 +8,61 @@ app = Flask(__name__)
 app.debug = True
 app.secret_key = "topsecret"
 
-all_results = {}
+class Calculator(object):
+    def __init__(self):
+        self.all_results = {}
 
-def is_a_number(arg):
-    try:
-        float(arg)
-    except ValueError:
-        return False
+    def calculate(self, sess_id, arg1_str, arg2_str, op):
+        if sess_id not in self.all_results:
+            self.all_results[sess_id] = []
 
-    return True
+        curr_results = self.all_results[sess_id]
 
-def calculate(arg1_str, arg2_str, op):
-    # Caller will catch ValueErrors raised by this
-    arg1 = float(arg1_str)
-    arg2 = float(arg2_str)
+        # Caller will catch ValueErrors raised by this
+        arg1 = float(arg1_str)
+        arg2 = float(arg2_str)
 
-    if op == "+":
-        return arg1 + arg2
-    elif op == "-":
-        return arg1 - arg2
-    elif op == "*":
-        return arg1 * arg2
-    elif op == "/":
-        return arg1 / arg2
-    else:
-        raise ValueError("Allowed operators are +, -, * and /")
+        if op == "+":
+            calc_result = arg1 + arg2
+        elif op == "-":
+            calc_result = arg1 - arg2
+        elif op == "*":
+            calc_result = arg1 * arg2
+        elif op == "/":
+            calc_result = arg1 / arg2
+        else:
+            raise ValueError("Allowed operators are +, -, * and /")
+
+        result = "{} {} {} = {}".format(arg1, op, arg2, calc_result)
+
+        curr_results.append(result)
+
+    def get_results(self, sess_id):
+        if sess_id not in self.all_results:
+            self.all_results[sess_id] = []
+
+        return self.all_results[sess_id]
+
+calculator = Calculator()
 
 @app.route("/")
 def calculator_page():
     errors = []
 
     if "id" not in session:
-        session["id"] = uuid.uuid4()
-
-    if session["id"] not in all_results:
-        all_results[session["id"]] = []
-
-    curr_results = all_results[session["id"]]
+        session["id"] = unicode(uuid.uuid4())
 
     try: 
         arg1 = request.args["arg1"]
         arg2 = request.args["arg2"]
         op = request.args["op"]
 
-        result = "{} {} {} = {}".format(
-            arg1, op, arg2, calculate(arg1, arg2, op)
-        )
-
-        curr_results.append(result)
+        calculator.calculate(session["id"], arg1, arg2, op)
 
     except KeyError:
         # We didn't get arguments and that's fine. (for instance, on the first
         # page load this will happen)
-        pass
+        print "Got KeyError"
 
     except ValueError as e:
         # We got some invalid values from the user who should be informed in 
@@ -68,7 +70,8 @@ def calculator_page():
         errors.append(str(e))
 
     return render_template(
-        "calculator.html", results=curr_results, errors=errors
+        "calculator.html", results=calculator.get_results(session["id"]),
+        errors=errors
     )
 
 if __name__ == "__main__":
